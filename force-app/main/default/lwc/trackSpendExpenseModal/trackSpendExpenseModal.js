46184ceff14c6ed26f2b77dfbe07cc07b0f89aef
Jc Red <john.carlo.d.red@gmail.com>
2026-02-16 09:26:32 +0800

    clean and fixed css animations

--- force-app/main/default/lwc/trackSpendExpenseModal/trackSpendExpenseModal.js
+++ force-app/main/default/lwc/trackSpendExpenseModal/trackSpendExpenseModal.js
@@ -1,11 +1,18 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 export default class TrackSpendExpenseModal extends LightningElement {
 
     @api isOpen = false;
+    @track isClosing = false;
 
     handleClose() {
-        this.dispatchEvent(new CustomEvent('close'));
+        this.isClosing = true;
+
+        const modal = this.template.querySelector('.slds-modal');
+        modal.addEventListener('animationend', () => {
+            this.isClosing = false;
+            this.dispatchEvent(new CustomEvent('close'));
+        }, { once: true });
     }
 
     handleSuccess() {
@@ -15,10 +22,19 @@ export default class TrackSpendExpenseModal extends LightningElement {
             }
         });
 
+        this.handleClose();
         this.dispatchEvent(new CustomEvent('success'));
     }
 
     handleError(event) {
         console.error('Error creating expense:', event.detail);
+    }
+
+    get modalClass() {
+        return `slds-modal slds-fade-in-open ${this.isClosing ? 'fade-out' : 'fade-in'}`;
+    }
+
+    get backdropClass() {
+        return `slds-backdrop ${this.isClosing ? '' : 'slds-backdrop_open'}`;
     }
 }
